99c105e41587e7b383fc80dcb105ff7bd6e77392
matveikulakovskiii <120181570+matveikulakovskiii@users.noreply.github.com>
2023-03-10 14:51:12 +0200

    Add files via upload

--- Ruutvõrrandi_lahendamine.py
+++ Ruutvõrrandi_lahendamine.py
@@ -3,96 +3,91 @@ from math import *
 import numpy as np
 import matplotlib.pyplot as plt 
 
-k=0
-def grafik():
-    ent_=float(ent.get())
-    ent2_=float(ent2.get())
-    ent3_=float(ent3.get())
-    x0=(-ent2_)/2*ent_ 
-    y0=ent_*x0*x0+ent2_*x0+ent3_
-    x=np.arange(x0-15,x0+15,1)#min,max,step
-    y=ent_*x*x+ent2_*x+ent3_
+
+
+def lahenda():
+    if ent.get()=="": 
+        ent.configure(bg="red")
+    else:
+        ent.configure(bg="lightblue")
+    if ent2.get()=="": 
+        ent2.configure(bg="red")
+    else:
+        ent2.configure(bg="lightblue")
+    if ent3.get()=="": 
+        ent3.configure(bg="red")
+    else:
+        ent3.configure(bg="lightblue")
+    if ent.get()!="" and ent2.get()!="" and ent3.get()!="":
+        ent_=float(ent.get())
+        ent2_=float(ent2.get())
+        ent3_=float(ent3.get())
+        D=ent2_*ent2_-4*ent_*ent3_
+        if D>0:
+            x1=round((-ent2_-sqrt(D))/(2*ent_),2)
+            x2=round((-ent2_+sqrt(D))/(2*ent_),2)
+            vas=f"X1={x1} \nX2={x2}"
+        elif D==0:
+            x1=round((-1*ent2_)/(2*ent_),2)
+            vas=f"X1={x1}"
+        else:
+            vas="Lahendust pole"
+        vastus.configure(text=vas)
+
+
+
+
+def graafik():
+    a_=float(ent.get())
+    b_=float(ent2.get())
+    c_=float(ent3.get())
+    x0=(-b_)/2*a_
+    y0=a_*x0*x0+b_*x0+c_
+    x=np.arange(x0-15,x0+15,1)
+    y=a_*x*x+b_*x+c_
     fig=plt.figure()
     plt.plot(x,y,'r-d')
-    plt.title('Ruutvõrrand')
+    plt.title("Ruutvõrrand")
     plt.ylabel('Y')
     plt.xlabel('X')
     plt.grid(True)
     plt.show()
 
 
-def klikkermaha(event):
-    global k
-    k-=1
-    btn.configure(text=k)
-
-def tekst_to_lbl(event):
-    t=ent.get()
-    lbl.configure(text=t)
-    ent.delete(0,END)
-
-def lahenda():
-    if ent.get()=='': ent.configure(bg='red')
-    else:
-        ent.configure(bg='Lightblue')
-    if ent2.get()=='': ent2.configure(bg='red')
-    else:
-        ent2.configure(bg='Lightblue')
-    if ent3.get()=='': ent3.configure(bg='red')
-    else:
-        ent3.configure(bg='Lightblue')
-    if ent.get()!='' and ent2.get()!='' and ent3.get()!='':
-        ent_=float(ent.get())
-        ent2_=float(ent2.get())
-        ent3_=float(ent3.get())
-        D=ent2_*ent2_-4*ent_*ent3_
-        if D>0:
-            x1=round((-1*ent2-sqrt(D))/(2*ent),2)
-            x2=round((-1*ent2-sqrt(D))/(2*ent),2)
-            vas=f'X1={x1} \nx2={x2}'
-        elif D==0:
-            x1=round(-1*ent2/(2*ent),2)
-            vas=f'X1={x1}'
-        else:
-            vastus='Lahendust pole'
-        vastus.configure(text=vas)
-
-
-
-
-
-
 
 
 aken=Tk()
-aken.geometry('700x300')
-aken.title('Ruutvõrrandid')
+aken.geometry("800x260")
+aken.title("Rootvõrrandid")
+f1=Frame(aken,width=650,height=260)
+f1.pack(side=TOP)
+f2=Frame(aken,width=650,height=200)
+f2.pack(side=BOTTOM)
 
 
-lbl=Label(aken,text='Ruutvõrrandi lahendamine',bg='gold',fg='#AA4A44',font='Arial 20',height=2,width=20)
-lbl2=Label(aken,text='x**2+',font='Arial 20',height=2,width=5)
-lbl3=Label(aken,text='x+',font='Arial 20',height=2,width=5)
-lbl4=Label(aken,text='=0',font='Arial 20',height=2,width=5)
-btn=Button(aken,text='Lahenda',font='Arial 24',relief=RAISED,command=lahenda)
-btn2=Button(aken,text='Graafika',font='Arial 24',relief=RAISED,command=lahenda)
-ent=Entry(aken,fg='blue',bg='Lightblue',width=5,font='Arial 20',justify=CENTER)
-ent2=Entry(aken,fg='blue',bg='Lightblue',width=5,font='Arial 20',justify=CENTER)
-ent3=Entry(aken,fg='blue',bg='Lightblue',width=5,font='Arial 20',justify=CENTER)
+lbl=Label(f1,text="Rootvõrrandite lahendamine",font="Times_New_Roman 26", fg="green",bg="lightblue")
+vastus=Label(f1,text="vastus", height=3,width=20,bg="yellow")
+ent=Entry(f1,font="Times_new_roman 26", fg="grey",bg="lightblue",width=3)
+lbl2=Label(f1,text="x**2+",font="Times_new_roman 26", fg="blue", padx=10)
+ent2=Entry(f1,font="Times_new_roman 26", fg="grey",bg="lightblue",width=3)
+lbl3=Label(f1,text="x+",font="Times_new_roman 26", fg="blue")
+ent3=Entry(f1,font="Times_new_roman 26", fg="grey",bg="lightblue",width=3)
+lbl4=Label(f1,text="=0",font="Times_new_roman 26", fg="blue")
+btn=Button(f1,text="Lahenda", font="Times_new_roman 26",bg="gold",command=lahenda)
+btn2=Button(f1,text="Graafik", font="Times_new_roman 26",bg="gold",command=graafik)
+btn_veel=Button(f2,text="Suurendada aken", font="Times_new_roman 26",bg="gold")
 
-btn.bind('<Button-1>') 
-btn.bind('<Button-3>',klikkermaha) 
-ent.bind('<Return>',tekst_to_lbl)
-ent2.bind('<Return>',tekst_to_lbl) 
-ent3.bind('<Return>',tekst_to_lbl) 
-lbl.pack(side=TOP) 
-btn.pack(side=RIGHT)
+
+
+lbl.pack(side=TOP)
+vastus.pack(side=BOTTOM)
 ent.pack(side=LEFT)
-lbl2.pack(side=LEFT) 
+lbl2.pack(side=LEFT)
 ent2.pack(side=LEFT)
-lbl3.pack(side=LEFT) 
+lbl3.pack(side=LEFT)
 ent3.pack(side=LEFT)
-lbl4.pack(side=LEFT) 
-grafik
-
-
+lbl4.pack(side=LEFT)
+btn2.pack(side=RIGHT)
+btn.pack(side=RIGHT)
+btn_veel.pack()
 aken.mainloop()
